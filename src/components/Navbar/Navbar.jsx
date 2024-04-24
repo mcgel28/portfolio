cb9797424298729ccadfdc63f345b6faed1710f2
Your Name <you@example.com>
2024-04-24 12:21:52 +0800

    reposition of navlist

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,8 +18,8 @@ function Navbar() {
           <img className="logo" src={logo} alt="logo.png"/>
           <ul id="menu" className={clicked ? "#menu active" : "menu"}>
             <li><Link to="hero-section" smooth={true} duration={500}>Home</Link></li>
-            <li><Link to="portfolio-section" smooth={true} duration={700}>Portfolio</Link></li>
-            <li><Link to="about-section" smooth={true} duration={600}>About</Link></li>
+            <li><Link to="about-section" smooth={true} duration={700}>About</Link></li>
+            <li><Link to="portfolio-section" smooth={true} duration={600}>Portfolio</Link></li>
             <li><Link to="contact-section" smooth={true} duration={800}>Contact</Link></li>
           </ul>
           <div id='mobile' onClick={handleClick}>
