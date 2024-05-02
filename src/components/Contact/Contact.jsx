925733265f92c0577f8fecb4faeaad0697084e12
Your Name <you@example.com>
2024-05-02 12:59:27 +0800

    update contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,28 +1,56 @@
-import React from "react";
-import handcoding from "../Assets/handcoding.png"
-import './Contact.css'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import handcoding from "../Assets/handcoding.png";
+import './Contact.css';
+import { MdInsertEmoticon } from "react-icons/md";
 
-function Contact() {
+
+
+const Contact = () => {
+    const form = useRef();
+  
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs
+        .sendForm('service_x7hvn5f', 'template_kep1a67', form.current, {
+          publicKey: 'HX8ymaxIeabDmoPa8',
+        })
+        .then(
+          () => {
+            // Display window alert on success
+            window.alert("Thank you for your message!");
+          },
+          (error) => {
+            console.log('FAILED...', error.text);
+          }
+        );
+    };
+  
+
+
     return (
         <div id="contact-section">
             <div className="contact">
                 <h1 className="contact-title">Get in Touch</h1>
-                    <div className="contact-container">
-                        <div className="contact-handcoding"> 
-                            <img className="handcoding" src={handcoding} alt="hand coding" />
-                        </div>
-                        <div className="form">
-                            <input type="text" name="name" placeholder=" Enter your name"></input>
-                            <input type="email" name="email" placeholder=" Enter your e-mail"></input>
-                            <textarea type="message" rows="6" name="message" placeholder=" What is your message?"></textarea>
-                            <div className="button-container">
+                <div className="contact-container">
+                    <div className="contact-handcoding"> 
+                        <img className="handcoding" src={handcoding} alt="hand coding" />
+                    </div>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <input type="text" name="from_name" placeholder=" Enter your name" required></input>
+                        <textarea id='message' rows="6" name="message" placeholder=" What is your message?" required></textarea>
+                        <p className='note'>Note: Include your e-mail in your message <MdInsertEmoticon /> </p>
+                        <div className="button-container">
                             <button type="submit" className="contact-button">SEND MESSAGE</button>
-                            </div>
+                            
+
                         </div>
-                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Contact;
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -9,27 +9,27 @@ function Portfolio() {
 
     return (
         <div id="portfolio-section">
-            <div class="portfolio">
-                <h1 class="port-title">Portfolio</h1>
-                <div class="work-list">
-                    <div class="work">
-                            <img class="portfolio-img"  src={highgear} alt="high gear motorcycles" />
-                        <div class="layer">
+            <div className="portfolio">
+                <h1 className="port-title">Portfolio</h1>
+                <div className="work-list">
+                    <div className="work">
+                            <img className="portfolio-img"  src={highgear} alt="high gear motorcycles" />
+                        <div className="layer">
                             <h3>High Gear Motorcycles<br/>Website</h3>
                             <a href="https://mcgel28.github.io/high-gear"><FaExternalLinkAlt /></a> 
                         </div>
                     </div>
 
-                    <div class="work">
-                        <img class="portfolio-img"  src={template} alt="rent motorcycle" />
-                        <div class="layer">
+                    <div className="work">
+                        <img className="portfolio-img"  src={template} alt="rent motorcycle" />
+                        <div className="layer">
                             <h3>In-Progress</h3>
                         </div>
                     </div>
                             
-                    <div class="work">
-                        <img class="portfolio-img"  src={template} alt="rent motorcycle" />
-                        <div class="layer">
+                    <div className="work">
+                        <img className="portfolio-img"  src={template} alt="rent motorcycle" />
+                        <div className="layer">
                             <h3>In-Progress</h3>
                         </div>
                     </div>
